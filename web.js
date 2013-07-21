6835731428001566881cfdcd16e2532b473058ad
Ubuntu <ubuntu@ip-172-31-37-212.us-west-2.compute.internal>
2013-07-21 19:47:34 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
+var infile = "index.html";
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFileSync(infile));
 });
 
 var port = process.env.PORT || 5000;
